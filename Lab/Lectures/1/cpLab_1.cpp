ab6a2d9d3a42aa8d931668ab39c582f308c271f2
Tanvir Mahtab <112494761+Tanv-ir@users.noreply.github.com>
2026-01-16 10:44:10 +0600

    Create cpLab_1.cpp

---
+++ Lab/Lectures/1/cpLab_1.cpp
@@ -0,0 +1,15 @@
+//This is a raw cpp/c++ file that can be compiled with GCC or other c/c++ compiler
+
+//Problem
+/*
+write a c++ code to determine the value of e^x using e = (x^0)/0! + (x^1)/1! + (x^2)/2! + ... + (x^n)/n!
+
+Also plot a graph of
+    i)e^x vs x where n is a fixed value
+   ii)e^x vs n where x is a fixed value
+*/
+
+//Solution
+
+//In this Solution I decided not to use the cmath library
+
