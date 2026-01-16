4571fc3f3c6383458e4ce0316e7bd44506fd0aab
Tanvir Mahtab <112494761+Tanv-ir@users.noreply.github.com>
2026-01-16 10:53:50 +0600

    Create cpLab_2.cpp

---
+++ Lab/Lectures/2/cpLab_2.cpp
@@ -0,0 +1,15 @@
+//This is a raw cpp/c++ file that can be compiled with GCC or other c/c++ compiler
+
+//problem
+/*
+Find the root of the following functions using Bi-section method.
+
+1. x*e^x = 1
+2. cos(x) - x*e^x = 0
+3. x*sin(x) + cos(x) = 0
+
+Make sure the root is currected upto n decimal places, where n can be declared by the users.
+Plot f(x) vs x graph for each functions.
+*/
+
+//Solution
