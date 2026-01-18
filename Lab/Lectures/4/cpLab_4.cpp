a67b9108ce2a672df9c0d49143952f782d773a58
Tanvir Mahtab <112494761+Tanv-ir@users.noreply.github.com>
2026-01-18 18:23:40 +0600

    Create cpLab_4.cpp

---
+++ Lab/Lectures/4/cpLab_4.cpp
@@ -0,0 +1,24 @@
+//Problem
+/*
+    f(x) = sin(x) + e^x .
+    
+    write a c++ code using Newton
+	  i) Forward Method
+     ii) Backward Method
+    iii) Central Method
+	
+    Plot f(x) Vs x
+*/
+
+//Solution
+
+#include<isotream>
+#include<cmath>
+#include<fstream>
+#include<iomanip>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
