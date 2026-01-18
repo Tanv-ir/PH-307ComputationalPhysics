6d17bf67469d36bfd9cf21d41d5e3def7429425f
Tanvir Mahtab <112494761+Tanv-ir@users.noreply.github.com>
2026-01-18 20:51:07 +0600

    Update cpLab_4.cpp

--- Lab/Lectures/4/cpLab_4.cpp
+++ Lab/Lectures/4/cpLab_4.cpp
@@ -1,12 +1,15 @@
+//This is a raw cpp/c++ file that can be compiled with GCC or other c/c++ compiler
+
 //Problem
+
 /*
-    f(x) = sin(x) + e^x .
+f(x) = sin(x) + e^x .
     
-    write a c++ code using Newton
-	  i) Forward Method
-     ii) Backward Method
-    iii) Central Method
-	
+write a c++ code using Newton
+	 1) Forward Method
+     2) Backward Method
+     3) Central Method
+	 
     Plot f(x) Vs x
 */
 
@@ -19,6 +22,70 @@
 
 using namespace std;
 
+//Given Function
+double func(double x){
+	double y;
+
+	y = sin(x) - exp(x);
+
+	return y;
+}
+
+//Forward Method
+double forward(double x, double h){
+	double y;
+
+	y = (func(x+h) - func(x))/h;
+
+	return y;
+}
+
+//Backward Method
+double backward(double x, double h){
+	double y;
+
+	y = (func(x) - func(x-h))/h;
+
+	return y;
+}
+
+//Central Method
+double central(double x, double h){
+	double y;
+
+	y = (func(x+h) - func(x-h))/(2*h);
+
+	return y;
+}
+
 int main(){
+	double x;
+	double h;
+
+	cout << "Enter a Value for x: ";
+	cin >> x;
+	cout << "Enter a Value for h: ";
+	cin >> h;
+	
+	cout << fixed << setprecision(4) << forward(x, h) << endl;
+	cout << fixed << setprecition(4) << backward(x, h) << endl;
+	cout << fixed << setprecision(4) << central(x, h) << endl;
+
+	double a;     //smallest input value for plot
+    double b;     //largest input value for plot
+
+	cout << "Enter the smallest value of x to draw a plot: ";
+	cin >> a;
+	cout << "Enter the largest value of x to draw a plot: ";
+	cin >> b;
+
+	ofstream plotdata("func.dat");    //creates an empty file name func.dat
+
+	for(double i = a; i <= b; i += 0.001){
+		plotdata << fixed << setprecition(4) << func(i) << "\t\t" << i << endl;
+	}
+
+	plotdata.close();    //prevents the func.dat file to take anymore inputs
+	
     return 0;
 }
